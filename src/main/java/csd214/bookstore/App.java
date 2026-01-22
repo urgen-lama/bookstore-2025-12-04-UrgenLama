3fd61527d6379c2e32a3950af4e5dcf4da24588c
urgen-lama <24046545@saultcollege.ca>
2026-01-22 12:25:22 -0500

    Lab 1: Rebranding

--- src/main/java/csd214/bookstore/App.java
+++ src/main/java/csd214/bookstore/App.java
@@ -16,7 +16,7 @@ public class App {
         populate();
         int choice = 0;
         while (choice != 99) {
-            System.out.println("\n***********************");
+            System.out.println("\nUrgen's Superstore");
             System.out.println(" 1. Add Items");
             System.out.println(" 2. Edit Items");
             System.out.println(" 3. Delete Items");
---
+++ src/main/java/csd214/bookstore/pojos/Stationery.java
@@ -0,0 +1,4 @@
+package csd214.bookstore.pojos;
+
+public class Stationery {
+}
